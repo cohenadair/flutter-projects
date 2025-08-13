0bafc4e09efe066da5f89e03a0daf1f588080493
Cohen Adair <cohenadair@gmail.com>
2025-08-13 13:56:12 -0400

    Uncomment Activity Log from tools projects

--- tools/projects.dart
+++ tools/projects.dart
@@ -1,6 +1,6 @@
 /// List of all projects and directories.
 final projects = [
   {"name": "adair-flutter-lib", "path": "../adair-flutter-lib"},
-  // {"name": "activity-log", "path": "../activity-log/mobile"},
+  {"name": "activity-log", "path": "../activity-log/mobile"},
   {"name": "anglers-log", "path": "../anglers-log/mobile"},
 ];
