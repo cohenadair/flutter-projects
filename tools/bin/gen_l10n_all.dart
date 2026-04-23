ca11f6d05eb9ad9de542809fb5df6781384a728a
Cohen Adair <cohenadair@gmail.com>
2026-04-22 20:25:45 -0400

    Remove tools (no longer needed with Claude Code)

--- tools/bin/gen_l10n_all.dart
+++
@@ -1,8 +0,0 @@
-import "../lib/projects.dart";
-
-Future<void> main() async {
-  for (var project in projects.values) {
-    await project.runCommand("flutter", ["gen-l10n"]);
-    print("Generated ${project.name} strings");
-  }
-}
